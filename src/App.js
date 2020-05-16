24af82e794d5e75abe1e696c2327929a26cbd2ae
Bumhan Yu <bumhan.yu@gmail.com>
2020-05-16 16:14:22 -0400

    initial commit: base styling && base dependency

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Blank = styled.div`
+  width: 100px;
+  height: 100px;
+  background: var(--pink);
+  color:#fff;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+class App extends React.Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <div style={{ width: '100vw', height: '100vh', display: 'flex', alignItems: 'center'}}>
+      <Blank>
+        <h1>test</h1>
+      </Blank>
+      </div>
+    )
+  }
 }
 
 export default App;
